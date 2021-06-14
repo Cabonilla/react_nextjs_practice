96fe783132e2c440308a882b0d8b46f6d2af7d58
cabonilla <christian.bonilla01@gmail.com>
2021-06-14 13:10:31 -0700

    Added Layout wrapper.

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,13 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+//our-domain.com/new-meetup
+
+function NewMeetupPage() {
+	function addMeetupHandler(enteredMeetupData) {
+		console.log(enteredMeetupData);
+	}
+	
+	return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+}
+
+export default NewMeetupPage;
