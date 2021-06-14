f7b8c0ad16bbbb40c36bfa92c7cec114fa6c6519
cabonilla <christian.bonilla01@gmail.com>
2021-06-14 13:51:07 -0700

    Added custom components and CSS modules for details.

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+function MeetupDetails() {
+	return (
+		<MeetupDetail 
+			image='https://images.unsplash.com/photo-1558497513-f0133e055abf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80'
+			title='First Cleanup'
+			address='12424 S. Vampire Ave. RU 29291'
+			description='Very dirty.'
+		/>
+	)
+}
+
+export default MeetupDetails
