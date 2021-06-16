0a8f2fc868b7324a06cdfd7964f9a902975f5024
cabonilla <christian.bonilla01@gmail.com>
2021-06-16 09:43:26 -0700

    Added HEAD metadata.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import { MongoClient } from 'mongodb';
+import Head from 'next/head';
+import { Fragment } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 const HomePage = (props) => {
 	return (
-		<MeetupList meetups={props.meetups} />
+		<Fragment>
+			<Head>
+				<title>React Cleanups</title>
+				<meta
+					name='description'
+					content='Browse a huge list of highly active React meetups!'
+				></meta>
+			</Head>
+			<MeetupList meetups={props.meetups} />
+		</Fragment>
 	)
 }
 
